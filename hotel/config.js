5f69388061d35ec6aca4cd3a79c1c9e34900ee65
anajuliafarias659-cloud <anajuliafarias659@gmail.com>
2026-01-15 01:50:45 -0300

    Delete hotel/config.js

--- hotel/config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-var SUPABASE_URL = "https://pdajixsoowcyhnjwhgpc.supabase.co",
-var SUPABASE_KEY = "sb_publishable_LatlFlcxk6IchHe3RNmfwA_9Oq4EsZw"
-
-function getUsuario() {
-  return JSON.parse(sessionStorage.getItem("usuario"));
-}
